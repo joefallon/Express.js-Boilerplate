04e7da9aee16758e817e611770805fdf92fe02cb
Joseph Fallon <joseph.t.fallon@gmail.com>
2018-02-21 14:08:06 -0800

    Several parameters were moved to the config file. The main index page was cleaned up.

--- src/config/main_config.js
+++ src/config/main_config.js
@@ -1,6 +1,9 @@
 "use strict";
 var config = {
-    port: 3030
+    port: 3030,
+    cookie_secret: 'j0f3uqxwaa6f00yj5l4soyd187bxm5iahh33zcil',
+    session_maxage: 24 * 60 * 60 * 1000,
+    listen_ip: '127.0.0.1'
 };
 module.exports = config;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbl9jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJtYWluX2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsSUFBTSxNQUFNLEdBQUc7SUFDWCxJQUFJLEVBQUUsSUFBSTtDQUNiLENBQUM7QUFFRixpQkFBUyxNQUFNLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbl9jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJtYWluX2NvbmZpZy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBQUEsSUFBTSxNQUFNLEdBQUc7SUFDWCxJQUFJLEVBQVksSUFBSTtJQUNwQixhQUFhLEVBQUcsMENBQTBDO0lBQzFELGNBQWMsRUFBRSxFQUFFLEdBQUcsRUFBRSxHQUFHLEVBQUUsR0FBRyxJQUFJO0lBQ25DLFNBQVMsRUFBTyxXQUFXO0NBQzlCLENBQUM7QUFFRixpQkFBUyxNQUFNLENBQUMifQ==
--- src/config/main_config.ts
+++ src/config/main_config.ts
@@ -1,5 +1,8 @@
 const config = {
-    port: 3030
+    port:           3030,
+    cookie_secret:  'j0f3uqxwaa6f00yj5l4soyd187bxm5iahh33zcil',
+    session_maxage: 24 * 60 * 60 * 1000, // 24 hours
+    listen_ip:      '127.0.0.1'
 };
 
 export = config;
